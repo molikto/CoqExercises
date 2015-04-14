8b5288644299d9d51c222c0d57e60fb60bbefef3
molikto <molikto@gmail.com>
2015-04-14 09:27:20 +0800

    no msg

--- software-foundations/Logic.v
+++ software-foundations/Logic.v
@@ -103,8 +103,29 @@ Definition implies_to_or := forall P Q:Prop,
   (P->Q) -> (~P\/Q).
   
 Goal peirce -> classic.
-Proof. unfold peirce. unfold classic. intros. apply (H _ (~P)).
+Proof.   unfold peirce.  unfold classic.  intros.  apply (H _ (~ P)).
+  intros.
+  set (fun (a: P) => (H0 (H1 a))).
+  apply H0 in f.
+  inversion f.Qed.
 
+Goal classic -> excluded_middle. unfold classic. unfold excluded_middle. intros H. intros. apply H. unfold not. intros. apply H0. right. intro. apply H0. left. apply H1. Qed.
 
+Goal excluded_middle -> de_morgan_not_and_not.
+Proof. unfold excluded_middle. unfold de_morgan_not_and_not. intros. assert (~ P -> Q). intros. set (H Q). destruct o. trivial. set (H0 (conj H1 H2)). inversion f. set (H P). destruct o. left. trivial. right. apply H1 in H2. trivial. Qed.
 
+Goal de_morgan_not_and_not -> implies_to_or.
+Proof. unfold de_morgan_not_and_not. unfold implies_to_or. intros. apply H. unfold not. intros. destruct H1. set (fun (a: P) => (H2 (H0 a))). apply H1 in f. inversion f. Qed.
 
+Goal implies_to_or -> peirce.
+Proof. unfold implies_to_or. unfold peirce. intros. assert (~P \/ P). apply H. intros. trivial. destruct H1. assert (False -> Q). intros. inversion H2. set (H0 (fun (a: P) => (H2 (H1 a)))). trivial. trivial. Qed.
+
+Theorem excluded_middle_irrefutable:  forall (P:Prop), ~ ~ (P \/ ~ P).  
+Proof. unfold not. intros. apply H. right. intro. set (H (or_introl H0)). inversion f. Qed.
+(** this is purely fantastic shit. **)
+
+Theorem false_beq_nat : forall n m : nat,
+     n <> m ->
+     beq_nat n m = false.
+Proof. induction n. destruct m. intros. set (H eq_refl). inversion f. intros. simpl. trivial. intros. destruct m. simpl. trivial. simpl. assert (n <> m). unfold not. intros. subst. set (H eq_refl). trivial.  apply IHn in H0. trivial. Qed.
+ 
