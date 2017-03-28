ab45b230f54eda0b4346152d108dc466176ee552
Minghao Liu <molikto@gmail.com>
2017-03-28 12:04:15 +0800

    some algebra

--- Algebra/Monoid.v
+++ Algebra/Monoid.v
@@ -1,6 +1,6 @@
 Require Import Coq.Lists.List.
 
-Class Monoid (A: Set) := {
+Class Monoid `(A: Set) := {
   unit: A;
   comp: A -> A -> A;
   left_unity: forall x: A, comp unit x = x;
@@ -27,5 +27,40 @@ end.
 
 Theorem comp_l_associtivity {A: Set} {m: Monoid A} (a b: list A) : 
 comp_l (app a b) = comp (comp_l a) (comp_l b).
-intros. induction a. simpl. rewrite left_unity. trivial. simpl. rewrite IHa. rewrite associtivity.
+Proof. intros. induction a. simpl. rewrite left_unity. trivial. simpl. rewrite IHa. rewrite associtivity.
 trivial. Qed.
+
+Class AbelianMonoid  `(A: Set) := {
+  AbelianMonoid_Monoid :> Monoid A;
+  commutative: forall a b: A, comp a b = comp b a
+}.
+
+Require Import Coq.Sorting.Permutation.
+
+
+Theorem abelian_comp {A: Set} { m: AbelianMonoid A} (l l': list A)
+(perm: Permutation l l'): comp_l l = comp_l l'.
+Proof. induction perm. trivial. simpl. rewrite IHperm.
+trivial. simpl. rewrite <- associtivity. rewrite <- associtivity.
+assert ((comp y x) = (comp x y)). rewrite commutative. trivial.
+rewrite H. trivial. rewrite IHperm1. rewrite IHperm2. trivial. Qed.
+
+Fixpoint monoid_pow {A: Set} { m: Monoid A} (x: A) (n: nat) := match n with
+  | 0 => unit
+  | S m => comp x (monoid_pow x m)
+end.
+
+Theorem monoid_pow_associtivity  {A: Set} {m: Monoid A} (a b: nat) (x: A) :
+monoid_pow x (a + b) = comp (monoid_pow x a) (monoid_pow x b).
+Proof. induction a. simpl. rewrite left_unity. trivial. simpl. rewrite IHa.
+rewrite associtivity. trivial. Qed.
+
+
+Theorem monoid_pow_commutative  {A: Set} {m: AbelianMonoid A} (a: nat) (x y: A) :
+monoid_pow (comp x y) a = comp (monoid_pow x a) (monoid_pow y a).
+Proof. induction a. simpl. rewrite left_unity. trivial. simpl. rewrite IHa.
+rewrite associtivity. rewrite associtivity. 
+assert (comp y (comp (monoid_pow x a) (monoid_pow y a)) = comp (monoid_pow x a) (comp y (monoid_pow y a))).
+rewrite commutative. rewrite associtivity. replace (comp (monoid_pow y a) y) with (comp y (monoid_pow y a)).
+trivial. rewrite commutative. trivial. rewrite H. trivial. Qed.
+
